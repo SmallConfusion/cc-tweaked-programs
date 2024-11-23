50d0445015727c090cfeebba0c23c7fc23837051
SmallConfusion <smallconfusion@protonmail.com>
2024-11-23 01:20:07 -0600

    rename install

--- miner.lua
+++ miner.lua
@@ -1,17 +1,18 @@
 local package = {
-    name = "yourpackage",
+    name = "miner",
 
     instdat = {
         repo_owner = "SmallConfusion",
         repo_name = "cct-miner",
         repo_ref = "main",
+        filemaps = {},
     },
 
     pkgType = "com.github",
     unicornSpec = "v1.0.0",
 }
 
-package.instdat.filemaps["init.lua"] = "miner/init.lua"
-package.instdat.filemaps["tracked_movement.lua"] = "miner/tracked_movement.lua"
+-- package.instdat.filemaps["init.lua"] = "miner/init.lua"
+-- package.instdat.filemaps["tracked_movement.lua"] = "miner/tracked_movement.lua"
 
 return package
