18c12d3cc871e44c70aac6ea85a409950b0b7c29
SmallConfusion <smallconfusion@protonmail.com>
2024-11-23 01:20:45 -0600

    Added files correctly

--- miner.lua
+++ miner.lua
@@ -12,7 +12,7 @@ local package = {
     unicornSpec = "v1.0.0",
 }
 
--- package.instdat.filemaps["init.lua"] = "miner/init.lua"
--- package.instdat.filemaps["tracked_movement.lua"] = "miner/tracked_movement.lua"
+package.instdat.filemaps["init.lua"] = "miner/init.lua"
+package.instdat.filemaps["tracked_movement.lua"] = "miner/tracked_movement.lua"
 
 return package
