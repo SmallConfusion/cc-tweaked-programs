c4594f67a0c18f973ead9c66df62bc3659535794
SmallConfusion <smallconfusion@protonmail.com>
2024-11-23 01:12:56 -0600

    filemaps

--- install.lua
+++ install.lua
@@ -5,14 +5,13 @@ local package = {
         repo_owner = "SmallConfusion",
         repo_name = "cct-miner",
         repo_ref = "main",
-
-        filemaps = {
-
-        },
     },
 
     pkgType = "com.github",
     unicornSpec = "v1.0.0",
 }
+
+package.instdat.filemaps["init.lua"] = "miner/init.lua"
+package.instdat.filemaps["tracked_movement.lua"] = "miner/tracked_movement.lua"
 
 return package
