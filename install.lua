05dbbe2c7547ebefc124a349e4580eb859708b15
SmallConfusion <smallconfusion@protonmail.com>
2024-11-23 01:10:13 -0600

    test unicornpkg install

---
+++ install.lua
@@ -0,0 +1,18 @@
+local package = {
+    name = "yourpackage",
+
+    instdat = {
+        repo_owner = "SmallConfusion",
+        repo_name = "cct-miner",
+        repo_ref = "main",
+
+        filemaps = {
+
+        },
+    },
+
+    pkgType = "com.github",
+    unicornSpec = "v1.0.0",
+}
+
+return package
