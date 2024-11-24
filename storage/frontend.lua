cae45d39a600ac572bc80c7ef6b2ed5176c82bb6
SmallConfusion <smallconfusion@protonmail.com>
2024-11-23 23:34:33 -0600

    Added proper get function

--- storage/frontend.lua
+++ storage/frontend.lua
@@ -14,45 +14,48 @@ function f.run()
 end
 
 function f.get()
+    local com = f.split(f.getCommand())
+    
+    local name = com[1]
+    local count = tonumber(com[2])
+
+    b.retrieveItems(b.matchItemName(name), count)
+end
+
+---@param commandName string
+---@return string command
+function f.getCommand(commandName)
     term.clear()
     term.setCursorPos(1, 1)
-    term.write("Get: ")
+    term.write(commandName)
+
+    local com = ""
 
     local _, key, _ = f.getKey()
-    local getName = ""
-    local getCount = ""
-    local gettingCount = false
 
     while key ~= keys.enter do
         local keyName = keys.getName(key)
 
-        if #keyName == 1 and not gettingCount then
-            term.write(keyName)
-            getName = getName .. keyName
-
-        elseif gettingCount then
-            local number = key - 48
-            local numString = tostring(number)
-
-            term.write(numString)
-            getCount = getCount .. numString
+        if #keyName == 1 then
+            com = com .. keyName
+        elseif key >= 48 and key <= 57 then
+            com = com .. tostring(key - 48)
+        elseif key == keys.underscore then
+            com = com .. "_"
+        elseif key == keys.space then
+            com = com .. " "
+        elseif key == keys.backspace then
+            com = com:sub(1, #com - 1)
         end
 
-        if key == keys.space then
-            term.write(" ")
-            gettingCount = true
-        end
+        term.clear()
+        term.setCursorPos(1, 1)
+        term.write(commandName .. com)
 
         _, key, _ = f.getKey()
     end
 
-    if #getCount > 0 then
-        getCount = tonumber(getCount)
-    else
-        getCount = 64
-    end
-
-    b.retrieveItems(b.matchItemName, getCount)
+    return com
 end
 
 function f.getKey()
@@ -61,6 +64,23 @@ end
 
 function f.cancel()
 
+end
+
+---@param s string
+---@param delim string
+---@return table
+function f.split(s, delim)
+    if sep == "nil" then
+        sep = "%s"
+    end
+
+    local t = {}
+
+    for str in s:gmatch("([^"..delim.."]+)") do
+        t[#t + 1] = str
+    end
+
+    return t
 end
 
 return f
