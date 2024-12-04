e339180eef204d900ec7161b7e6f03365ea42e4f
SmallConfusion <smallconfusion@protonmail.com>
2024-12-04 04:44:23 -0600

    Add support for running refresh in background

--- storage/init.lua
+++ storage/init.lua
@@ -6,17 +6,25 @@ local function runStore()
 
     while true do
         ---@diagnostic disable-next-line: undefined-field
-        if b.cacheOutdated then
-            b.refreshCache()
-        end
-
         b.storeAll()
     end
 end
+
+local function runRefresh()
+    local b = require("storage.backend")
+    while true do
+        if b.cacheOutdated then
+            b.refreshCache()
+        else
+            os.sleep(0.5)
+        end
+    end
+end
+
 
 if pocket then
     f.run()
 else
     local b = require("storage.backend")
-    parallel.waitForAll(runStore, f.run, b.runWireless)
+    parallel.waitForAll(runStore, runRefresh, f.run, b.runWireless)
 end
